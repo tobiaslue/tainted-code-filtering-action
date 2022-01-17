1186c000e6be675c4bdc3c3c73fcd6ea0d81a45d
Tobias <lutobias@student.ethz.ch>
2022-01-17 17:55:24 +0100

    Add sudo to make

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ time=$(date)
 echo "::set-output name=time::$time"
 
 sudo cmake -DCAMKE_C_COMPILER=/usr/local/clang_9.0.0/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/clang_9.0.0/bin/clang++ -DLLVM_DIR=/opt/llvm -DLIBCXX_PATH=/opt/llvm/lib -DJSONCPP_PATH=/usr/local/json
-make 
+sudo make 
