9c8cc32ccd5109e9a7c3cbb5b5b863002730795b
Tobias <lutobias@student.ethz.ch>
2022-01-17 17:24:13 +0100

    Change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,4 +4,5 @@ echo "Hello $1"
 time=$(date)
 echo "::set-output name=time::$time"
 
+cd tainted-code-filtering-action
 cmake -DCAMKE_C_COMPILER=/usr/local/clang_9.0.0/bin/clang -DCMAKE_CXX_COMPILER=/usr/local/clang_9.0.0/bin/clang++ -DLLVM_DIR=/opt/llvm -DLIBCXX_PATH=/opt/llvm/lib -DJSONCPP_PATH=/usr/local/json
