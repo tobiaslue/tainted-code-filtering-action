3a19fbc47fa158c8e0bd187eabf1708973f45e86
Tobias <lutobias@student.ethz.ch>
2022-01-29 16:56:02 +0100

    Add clang3.9

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ sudo git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
 cd tainted-code-filtering-action
 branch=${GITHUB_REF##*/}
 sudo git checkout $branch
-sudo apt-get install python-clang-3.4
+sudo apt-get install python-clang-3.9
 
 
 # export PYTHONPATH=/home/tobiaslue/llvm-project/clang/bindings/python
