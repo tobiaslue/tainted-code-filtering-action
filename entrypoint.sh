da8124ccfc26b83a60c17a5210bb1c3eb0bc4fa2
Tobias <lutobias@student.ethz.ch>
2022-01-29 11:06:01 +0100

    Experiment

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,8 @@
 sudo git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
 cd tainted-code-filtering-action
 branch=${GITHUB_REF##*/}
+echo $(ls)
 echo $(git branch)
 echo $(git diff HEAD^..HEAD)
-echo $(git diff --name-only origin/master origin/${GITHUB_HEAD_REF})
+echo $(git diff --name-only master ${GITHUB_HEAD_REF})
 echo $(git diff master $branch)
