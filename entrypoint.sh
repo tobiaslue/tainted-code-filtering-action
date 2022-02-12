0a2719f6dcacdb36af11407f12f1d7ba182f54a8
Tobias <lutobias@student.ethz.ch>
2022-02-12 16:29:44 +0100

    Try cmake

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
+set -o xtrace
 
 # Execute here
-sudo git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
+sudo chown -R docker .
+chmod -R 700 .
+mkdir tmp
+git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
 cd tainted-code-filtering-action
 branch=${GITHUB_REF##*/}
-sudo git checkout $branch
+#sudo git checkout $branch
+git checkout test_branch
+python3 ../parse-diff.py master test_branch
 
-sudo python3 /home/docker/parse-diff.py master $branch
-
-sudo cmake\
+echo $(ls)
+echo {} > /home/docker/tainted_functions.json
+cmake\
     -DCMAKE_CXX_COMPILER=/opt/llvm/bin/clang++1\
-    && sudo make
+    && make
