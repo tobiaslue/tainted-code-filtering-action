92a43494b1cc89df0017751656bf20e9a88c9d2a
Tobias <lutobias@student.ethz.ch>
2022-02-12 16:32:56 +0100

    Add sudo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,18 +2,18 @@
 set -o xtrace
 
 # Execute here
-sudo chown -R docker .
-chmod -R 700 .
-mkdir tmp
-git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
+# sudo chown -R docker .
+# chmod -R 700 .
+# mkdir tmp
+sudo git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
 cd tainted-code-filtering-action
 branch=${GITHUB_REF##*/}
 #sudo git checkout $branch
-git checkout test_branch
-python3 ../parse-diff.py master test_branch
+sudo git checkout test_branch
+sudo python3 ../parse-diff.py master test_branch
 
 echo $(ls)
 echo {} > /home/docker/tainted_functions.json
-cmake\
+sudo cmake\
     -DCMAKE_CXX_COMPILER=/opt/llvm/bin/clang++1\
-    && make
+    && sudo make
