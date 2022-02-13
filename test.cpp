5a81df7a91c5035e0be50984cb66b76d62c6585b
Tobias <lutobias@student.ethz.ch>
2022-02-13 18:07:18 +0100

    Change program

--- test.cpp
+++ test.cpp
@@ -1,10 +1,10 @@
+int test(int x){
+    int z = 5;
+    return z + 2;
+}
+
 int main(){
     int x = 6;
     int y = test(x);
     return y;
-}
-
-int test(int x){
-    int z = 5;
-    return z + 2;
 }
