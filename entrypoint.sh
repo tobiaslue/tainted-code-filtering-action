cfc84da86c90e44448c45ad7b25e2a738e17dda4
Tobias <lutobias@student.ethz.ch>
2022-02-15 12:31:28 +0100

    Change entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,14 @@ mkdir /home/docker/tmp
 sudo git clone https://github.com/tobiaslue/tainted-code-filtering-action.git
 cd tainted-code-filtering-action
 branch=${GITHUB_REF##*/}
-sudo git checkout $branch
-# sudo git checkout test
-sudo python3 /home/docker/parse-diff.py master $branch
+# sudo git checkout $branch
+sudo git checkout test
+sudo python3 /home/docker/parse-diff.py master test
 
 
 echo {} > /home/docker/tainted_functions.json
+echo "1" > /home/docker/counter.txt
+
 sudo cmake\
     -DCMAKE_CXX_COMPILER=/opt/llvm/bin/clang++1\
     && sudo make
@@ -21,3 +23,10 @@ sudo cmake\
 echo {} > /home/docker/tainted_functions.json
 ./TestLibrary
 cat /home/docker/tainted_functions.json
+
+echo "2" > /home/docker/counter.txt
+
+sudo make clean
+sudo make
+
+# Create a file with value 0 for the first compile. Change to 1 after the first execution. in the wrapper, check the value of the file, and change the passes based on the value.
