b097b3ce2e965bb2a0da9aeb467a51cb3018945c
Tobias Lüscher <tobiaslue@gmail.com>
2022-02-15 18:19:34 +0100

    Try to build with user input

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,17 +15,18 @@ sudo python3 /home/docker/parse-diff.py master test
 
 echo {} > /home/docker/tainted_functions.json
 echo "1" > /home/docker/counter.txt
-echo "$@"
+
 $1 && $2
 
 echo {} > /home/docker/tainted_functions.json
-./TestLibrary
+
+$3
+
 cat /home/docker/tainted_functions.json
 
 echo "2" > /home/docker/counter.txt
 
-sudo make clean
-sudo make
-./TestLibrary
+$4 && $2 && $3
+
 cat /home/docker/tainted_lines.json
 # Create a file with value 0 for the first compile. Change to 1 after the first execution. in the wrapper, check the value of the file, and change the passes based on the value.
