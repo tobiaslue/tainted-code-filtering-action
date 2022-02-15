bc9b141578204e93e216269fb3e8982e167568be
Tobias Lüscher <tobiaslue@gmail.com>
2022-02-15 17:59:53 +0100

    Try to build with user input

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,9 +16,8 @@ sudo python3 /home/docker/parse-diff.py master test
 echo {} > /home/docker/tainted_functions.json
 echo "1" > /home/docker/counter.txt
 
-# chmod +x ./$1
-sudo ${{ github.repository }}/$1
-
+$1
+$2
 
 echo {} > /home/docker/tainted_functions.json
 ./TestLibrary
